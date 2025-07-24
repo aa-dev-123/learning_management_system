3a55d654217c499521c524e7389d8acd693f74e6
Alishba Atther <alishbaather321@gmail.com>
2025-07-24 21:01:09 +0500

    run course create migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_24_134304) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_24_143002) do
+  create_table "courses", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
