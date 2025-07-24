f225f6e03646eec5ce69266fa9a5c9e677b73e4a
Alishba Atther <alishbaather321@gmail.com>
2025-07-25 02:34:53 +0500

    created join table for course and category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_24_213039) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_24_213224) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -43,6 +43,15 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_24_213039) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "categories_courses", force: :cascade do |t|
+    t.integer "category_id"
+    t.integer "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_categories_courses_on_category_id"
+    t.index ["course_id"], name: "index_categories_courses_on_course_id"
   end
 
   create_table "courses", force: :cascade do |t|
