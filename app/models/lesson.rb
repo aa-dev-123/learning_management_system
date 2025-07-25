7f8649430ed3dd75d82f19e580293b946750c84a
Alishba Atther <alishbaather321@gmail.com>
2025-07-25 18:04:18 +0500

    added some helper methods and styling fix

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,6 +1,16 @@
 class Lesson < ApplicationRecord
-  has_one_attached :video
+  has_one_attached :video do |attachable|
+    attachable.variant :thumb, resize_to_limit: [500,500]
+  end
 
   belongs_to :course
   has_many :lesson_users, dependent: :destroy
+
+  def next_lesson
+    course.lessons.where("position > ?", position).order(:position).first
+  end
+
+  def previous_lesson
+    course.lessons.where("position < ?", position).order(:position).last
+  end
 end
