68697cfe617e0abd26ba8bd22de61f470ed5f41f
Alishba Atther <alishbaather321@gmail.com>
2025-07-26 12:00:00 +0500

    resolve issue for button not taking to the stripe website

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,21 +1,21 @@
 class CheckoutsController < ApplicationController
   before_action :authenticate_user!
 
-  stripe_secret_key = Rails.app.credentials.dig(:stripe, :secret_key)
+  stripe_secret_key = Rails.application.credentials.dig(:stripe, :secret_key)
   Stripe.api_key = stripe_secret_key
 
   def create
     course = Course.find(params[:course_id])
 
-    Stripe::Checkout::Session.create(
+    session = Stripe::Checkout::Session.create(
       mode: "payment",
       line_items: [{
-        price: coursee.stripe_price_id,
+        price: course.stripe_price_id,
         quantity: 1
       }],
       success_url: request.base_url + "/courses/#{course.id}",
       cancel_url: request.base_url + "/courses/#{course.id}",
-      automatic_tax: { enabled: true },
+      # automatic_tax: { enabled: true },
       customer_email: current_user.email,
       metadata: {
         course_id: course.id
